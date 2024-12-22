d6f7da43cd2eaa119587e106cd1a63236ab928d5
TaDaa <trevor002@gmail.com>
2024-12-21 20:14:49 -0500

    Lua: Add base62 key_reducer. This will be used for generating smaller keys based on highlight names.

---
+++ lua/vimade/util/key_reducer.lua
@@ -0,0 +1,45 @@
+local MATH_FLOOR = math.floor
+
+-- generates keys at base 62
+local KEYS = {0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}
+local TOTAL_KEYS = #KEYS
+
+return function()
+  local id = 1
+  local cache = {}
+  local key_reducer = {}
+
+  key_reducer.reduce = function(input)
+    local key = cache[input]
+    if key then
+      return key
+    end
+    key = ''
+    local i = id
+    local r = 0
+    while i > 0 do
+      r = i % TOTAL_KEYS
+      i = MATH_FLOOR(i / TOTAL_KEYS)
+      key = KEYS[r + 1] .. key
+    end
+    cache[input] = key
+    id = id + 1
+    return key
+  end
+  key_reducer.reduce_ipairs = function(input)
+    local output = ''
+    for _, value in ipairs(input) do
+      output = output .. key_reducer.reduce(value) .. ','
+    end
+    return output:sub(1,-2)
+  end
+  key_reducer.reduce_pairs_key = function(input)
+    local output = ''
+    for key, value in pairs(input) do
+      output = output .. key_reducer.reduce(key) .. ','
+    end
+    return output:sub(1,-2)
+  end
+
+  return key_reducer
+end
