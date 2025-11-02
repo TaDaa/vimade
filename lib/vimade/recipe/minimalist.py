dafb4b857f25cc19611cb973d25d0ed20bc42127
TaDaa <trevor002@gmail.com>
2025-11-02 00:41:26 -0400

    feat(VimFocus): Move minimalist to leverage component

--- lib/vimade/recipe/minimalist.py
+++ lib/vimade/recipe/minimalist.py
@@ -7,6 +7,7 @@ from vimade.style.value import animate as ANIMATE
 from vimade.style.value import condition as CONDITION
 from vimade.style import fade as FADE
 from vimade.style import tint as TINT
+from vimade.style.component import Component
 from vimade.style.exclude import Exclude
 from vimade.style.include import Include
 from vimade.util import type as TYPE
@@ -24,73 +25,85 @@ def animate_minimalist(config):
     'direction': config.get('direction'),
   }
   return [
-    TINT.Tint(
-      condition = condition,
-      value = ANIMATE.Tint(**TYPE.extend({}, animation, {
-        'to': TINT.Default().value()
-      }))
-    ),
-    Exclude(
-      condition = condition,
-      value = config.get('exclude_names'),
+    Component('Pane',
       style = [
-        FADE.Fade(
-          value = ANIMATE.Number(**TYPE.extend({}, animation, {
-            'to': FADE.Default().value(),
-            'start': 1
+        TINT.Tint(
+          condition = condition,
+          value = ANIMATE.Tint(**TYPE.extend({}, animation, {
+            'to': TINT.Default().value()
           }))
+        ),
+        Exclude(
+          condition = condition,
+          value = config.get('exclude_names'),
+          style = [
+            FADE.Fade(
+              value = ANIMATE.Number(**TYPE.extend({}, animation, {
+                'to': FADE.Default().value(),
+                'start': 1
+              }))
+            )
+          ]
+        ),
+        Include(
+          condition = condition,
+          value = config.get('no_visibility_names'),
+          style = [
+            FADE.Fade(
+              value = ANIMATE.Number(**TYPE.extend({}, animation, {
+                'to': 0,
+                'start': 1
+              }))
+            )
+          ]
+        ),
+        Include(
+          condition = condition,
+          value = config.get('low_visibility_names'),
+          style = [
+            FADE.Fade(
+              value = ANIMATE.Number(**TYPE.extend({}, animation, {
+                'to': config.get('low_visibility_fadelevel'),
+                'start': 1
+              }))
+            )
+          ]
         )
       ]
-    ),
-    Include(
-      condition = condition,
-      value = config.get('no_visibility_names'),
-      style = [
-        FADE.Fade(
-          value = ANIMATE.Number(**TYPE.extend({}, animation, {
-            'to': 0,
-            'start': 1
-          }))
-        )
-      ]
-    ),
-    Include(
-      condition = condition,
-      value = config.get('low_visibility_names'),
-      style = [
-        FADE.Fade(
-          value = ANIMATE.Number(**TYPE.extend({}, animation, {
-            'to': config.get('low_visibility_fadelevel'),
-            'start': 1
-          }))
-        )
-      ]
-    ),
+    )
   ]
 
 def minimalist(config):
   condition = config.get('condition') 
   return [
-      TINT.Default(**config),
-      Exclude(
-        condition = condition,
-        value = config.get('exclude_names'),
-        style = [FADE.Default()]
-      ),
-      Include(
-        condition = condition,
-        value = config.get('no_visibility_names'),
-        style = [FADE.Fade(value = 0)]
-      ),
-      Include(
-        condition = condition,
-        value = config.get('low_visibility_names'),
-        style = [FADE.Fade(value = config.get('low_visibility_fadelevel'))]
-      ),
+      Component('Pane',
+        style = [
+          TINT.Default(**config),
+          Exclude(
+            condition = condition,
+            value = config.get('exclude_names'),
+            style = [FADE.Default()]
+          ),
+          Include(
+            condition = condition,
+            value = config.get('no_visibility_names'),
+            style = [FADE.Fade(value = 0)]
+          ),
+          Include(
+            condition = condition,
+            value = config.get('low_visibility_names'),
+            style = [FADE.Fade(value = config.get('low_visibility_fadelevel'))]
+          )
+        ]
+      )
     ]
 
 # @param **kwargs {
   # @optional animate: boolean = false
+  # @optional exclude_names: string[] = ['LineNr', 'LineNrBelow', 'LineNrAbove', 'WinSeparator', 'EndOfBuffer']
+  # @optional no_visibility_names: string[] = ['LineNr', 'LineNrBelow', 'LineNrAbove', 'EndOfBuffer']
+  # @optional low_visibility_names: string[] = ['WinSeparator']
+  # @optional low_visibility_fadelevel: number = 0.1
   # @optional ease: EASE = ANIMATE.DEFAULT_EASE
   # @optional delay: number = ANIMATE.DEFAULT_DELAY
   # @optional duration: number = ANIMATE.DEFAULT_DURATION
@@ -98,10 +111,11 @@ def minimalist(config):
 # }
 def Minimalist(**kwargs):
   config = TYPE.shallow_copy(kwargs)
-  config['exclude_names'] = config['exclude_names'] if config.get('exclude_names') != None else EXCLUDE_NAMES
-  config['no_visibility_names'] = config['no_visibility_names'] if config.get('no_visibility_names') != None else NO_VISIBILITY_NAMES
-  config['low_visibility_names'] = config['low_visibility_names'] if config.get('low_visibility_names') != None else LOW_VISIBILITY_NAMES
-  config['low_visibility_fadelevel'] = config['low_visibility_fadelevel'] if config.get('low_visibility_fadelevel') != None else 0.2
+  config['condition'] = config.get('condition', CONDITION.INACTIVE)
+  config['exclude_names'] = config.get('exclude_names', EXCLUDE_NAMES)
+  config['no_visibility_names'] = config.get('no_visibility_names', NO_VISIBILITY_NAMES)
+  config['low_visibility_names'] = config.get('low_visibility_names', LOW_VISIBILITY_NAMES)
+  config['low_visibility_fadelevel'] = config.get('low_visibility_fadelevel', 0.1)
   return {
     'style': animate_minimalist(config) if config.get('animate') else minimalist(config),
     'linkwincolor': [] if GLOBALS.is_nvim else [x for x in config['no_visibility_names'] if x != 'VimadeWC'],
